7ed29d5f17d57572cccf6dae95e315c887fb998e
rajeshprema <rajeshprema774@gmail.com>
2024-04-26 22:40:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
+import { Route, Routes } from 'react-router';
 import './App.css';
+import Register from "./Components/Register.js";
+import Login from "./Login";
+import Home from "./Home"
+
+
+// import Registration from "./Components/Registration/registration"
+ import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 function App() {
+
+  
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ marginTop : '-3.5rem'}}></div>
+    
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
+      </Routes>
     </div>
   );
-}
 
+}
 export default App;
+// https://github.com/AkshataGanbote/Registration_Login_Form_MERN_Stack/tree/main/frontend/src/components
